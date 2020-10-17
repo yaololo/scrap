f9a386a1e7ec7dbcbb2712ae9fe7c3eb1421327f
yongming <zhuangyongming1900@gmail.com>
2020-10-18 00:00:34 +0800

    Add scraped experience fiels and update experience script

--- g-experience.py
+++ g-experience.py
@@ -15,14 +15,12 @@ driver = webdriver.Chrome(chrome_path)
 driver.get("https://www.linkedin.com")
 time.sleep(2)
 
-
 driver.find_element_by_xpath("""//*[@id="session_key"]""").send_keys(userid)
 driver.find_element_by_xpath(
     """//*[@id="session_password"]""").send_keys(password)
 driver.find_element_by_class_name("sign-in-form__submit-button").click()
 
 print("successfully login")
-
 
 def get_top_block(top_block):
   uls = top_block.find_elements_by_tag_name("ul")
@@ -51,11 +49,20 @@ def get_education(education_block):
     school_name = education_info.find_element_by_tag_name("h3").text
     school_names.append(school_name)
 
-    duration = education_info.find_element_by_xpath("""div//p[contains(@class, 'pv-entity__dates t-14 t-black--light t-normal')]""").find_elements_by_tag_name("span")[1].text.replace("–", "-")
+    duration = "na"
+    with_duration = education_info.find_elements_by_xpath("""div//p[contains(@class, 'pv-entity__dates t-14 t-black--light t-normal')]""")
+    if len(with_duration) > 0:
+      duration = with_duration[0].find_elements_by_tag_name("span")[1].text.replace("–", "-")
+
     durations.append(duration)
 
-    degree_name = education_info.find_element_by_xpath("""div//div""").find_elements_by_tag_name("p")[0].find_elements_by_tag_name("span")[1].text
-    field_of_study = education_info.find_element_by_xpath("""div//div""").find_elements_by_tag_name("p")[1].find_elements_by_tag_name("span")[1].text
+    degree_name = "na"
+    field_of_study = "na"
+    with_degree = education_info.find_element_by_xpath("""div//div""").find_elements_by_tag_name("p")
+    if len(with_degree) > 0:
+      degree_name = with_degree[0].find_elements_by_tag_name("span")[1].text
+      if len(with_degree) > 1:
+        field_of_study = with_degree[1].find_elements_by_tag_name("span")[1].text
 
     degree_names.append(degree_name)
     fields_of_study.append(field_of_study)
@@ -67,14 +74,16 @@ def get_experience(url):
   driver.get(url)
 
   # Scroll page down to bottom to load all DOM element
-  driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
-  time.sleep(2)
-  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+  driver.execute_script("window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });")
   time.sleep(2)
 
   # Click to expand industrial skills section
-  driver.find_element_by_xpath("""//button[@aria-controls='skill-categories-expanded']""").click()
+  click_button_script = "var buttons = document.getElementsByClassName('pv-profile-section__card-action-bar pv-skills-section__additional-skills artdeco-container-card-action-bar artdeco-button artdeco-button--tertiary artdeco-button--3 artdeco-button--fluid'); if(buttons.length>0) {buttons[0].click()}"
+
+  driver.execute_script(click_button_script)
   time.sleep(1)
+
+  print("after click on button")
 
   row = [url]
   main_element = driver.find_element_by_xpath("""//main[contains(@class, 'core-rail')]""")
@@ -86,8 +95,12 @@ def get_experience(url):
   row += top_block_information
 
 
-  education_section = main_element.find_element_by_xpath("""//section[@id="education-section"]""")
-  education_information =get_education(education_section)
+  education_section = main_element.find_elements_by_xpath("""//section[@id="education-section"]""")
+
+  # if no education block, put na for school_name, durations, degree_names, and fields_of_study
+  education_information = ["na", "na", "na", "na"]
+  if len(education_section) > 0:
+    education_information =get_education(education_section[0])
 
   # Add education information to list
   row += education_information
@@ -102,32 +115,38 @@ def get_experience(url):
   # Add education information to list
   row.append( "|".join(skills))
 
-  return row
   print(row)
+  return row
 
-# def write_employee_experience(index):
-#     with open(f'./google/employee_file{index}.csv', encoding='utf-8-sig') as csv_file_open:
-#         csv_reader = csv.reader(csv_file_open, delimiter=',')
+def write_employee_experience(index):
+    with open(f'./google/employee_file{index}.csv', encoding='utf-8-sig') as csv_file_open:
+        csv_reader = csv.reader(csv_file_open, delimiter=',')
 
-#         with open(f"./employee_experience/employee_experience{index}.csv", mode='w', newline='', encoding='utf-8-sig') as csv_file_write:
-#             fieldnames = ['employee_url','name','current_job_title', "current_location", "connections", "schools", "durations", "degree_name", "field_of_study", "industrial_skills"]
-#             writer = csv.writer(csv_file_write, delimiter=',',
-#                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        with open(f"./employee_experience/employee_experience{index}.csv", mode='w', newline='', encoding='utf-8-sig') as csv_file_write:
+            fieldnames = ['employee_url','name','current_job_title', "current_location", "connections", "schools", "durations", "degree_name", "field_of_study", "industrial_skills"]
+            writer = csv.writer(csv_file_write, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-#             writer.writerow(fieldnames)
+            writer.writerow(fieldnames)
 
-#             count = 0
-#             for row in csv_reader:
-#                 if count > 0:
-#                     print("user_link: ", row[0])
-#                     employee_experience = get_experience(row[0])
-#                     writer.writerow(employee_experience)
-#                     print("================== write user completed ===================")
+            count = 0
+            for row in csv_reader:
+                if count > 0:
+                    print("user_link: ", row[0])
+                    employee_experience = get_experience(row[0])
+                    writer.writerow(employee_experience)
+                    print("================== write user completed ===================")
 
-#                 count += 1
+                count += 1
 
-#     print("================== write file completed ===================")
+    print("================== write file completed ===================")
 
 
 # get_experience("file:///C:/Users/yongming/side_project/scrap/uni-code.html")
-get_experience("https://www.linkedin.com/in/reviyabut/")
+# get_experience("file:///C:/Users/yongming/side_project/scrap/test.html")
+# get_experience("https://www.linkedin.com/in/raunak-bhandari/")
+
+# for i in range(50):
+#     if i > 5:
+#         write_employee_experience(i+1)
+write_employee_experience(1)
